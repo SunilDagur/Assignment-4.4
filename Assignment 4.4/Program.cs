4841871ee1cf9496af2554ca6ec4155cbff1bfc8
Sunil Dagur <sunildagur566@gmail.com>
2023-09-16 18:51:02 +0530

    Add project files.

---
+++ Assignment 4.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._4
+{
+    using System;
+
+    public class Temperature
+    {
+        private double celsius;
+
+        public double Celsius
+        {
+            get { return celsius; }
+            set { celsius = value; }
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                
+                return (celsius * 9 / 5) + 32;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temperature = new Temperature();
+            temperature.Celsius = 25; 
+
+            Console.WriteLine($"Celsius: {temperature.Celsius}");
+            Console.WriteLine($"Fahrenheit: {temperature.Fahrenheit}");
+        }
+    }
+
+}
